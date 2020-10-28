baf382d03e723fc6fa28c73bc1121100767b3927
djviodes <djviodes26@gmail.com>
2020-10-28 08:40:14 -0700

    Finished useLocalStorage and began working on useDarkMode

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,9 @@
+import useLocalStorage from './useLocalStorage';
+
+const useDarkMode = () => {
+    const [darkModeOn, setDarkModeOn] = useLocalStorage('on');
+
+    return [];
+}
+
+export default useDarkMode; 
